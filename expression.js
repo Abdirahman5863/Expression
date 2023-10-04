c1224a737d15fda94c804afeac2ba2c0aab663f5
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2023-10-04 14:44:57 +0300

    implemented implicit binding with this keyword

--- expression.js
+++ expression.js
@@ -18,3 +18,15 @@ console.log(
     obj
 )
 
+// Implicit binding - this keyword
+
+const person ={
+    name: 'john',
+    age:23,
+    display: function()
+    {
+        return 'My  name  is ' + this.name + ' and  I  am ' + ' years  old';
+    }
+}
+
+console.log(person.display());
